fbefe32b3cbf349520bb8f6be109f4ab1ccda98e
Alexandr Zhygailo <alexandr.zhygailo@mev.com>
2016-08-19 11:30:05 +0300

    Fixed project source url

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         ],
     author='Alexander Zhygailo',
     author_email='alexander777vz@gmail.com',
-    url='https://github.com/azsoftware/wtf-tinymce',
+    url='https://github.com/azsoftware/wtf_tinymce',
     keywords='wtf-tinymce',
     packages=find_packages(),
     include_package_data=True,
